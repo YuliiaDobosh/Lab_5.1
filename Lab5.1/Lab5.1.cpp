f61ea5aceaab6e13b35335e1cc45e8ef736cd37c
YuliiaDobosh <yuliia.dobosh.it.2021.lpnu.ua>
2021-10-18 18:09:22 +0300

    second task

--- Lab5.1/Lab5.1.cpp
+++ Lab5.1/Lab5.1.cpp
@@ -9,11 +9,11 @@ int main()
 
 	cout << "s = "; cin >> s;
 	cout << "t = "; cin >> t;
-	double m = (f(1,t*t, s) + f(t, s*s,1))/( 1+ f(1,t*s,1)* f(1, t * s, 1));
+	double m = ((f(1, pow(t, 2), s) + f(t, pow(s, 2), 1)) / (1 + pow(f(1, t * s, 1), 2)));
 	cout << "m = " << m << endl;
 	return 0;
 }
 double f(const double a, const double b, const double c) // визначення
 {
-	return (sin (a*b*c)*sin(a * b * c)) / ((a*a)+(b*b)+(c*c));
+	return (sin(a * b * c) * sin(a * b * c)) / ((a * a) + (b * b) + (c * c));
 }
